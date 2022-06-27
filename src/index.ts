ec8b3fefc80899d5fd2407bafb5039023244c52e
Yuri Carlos <yuryshadow@gmail.com>
2022-06-27 11:45:09 -0300

    Adicionando o typescript e incrementando a forma de esenvolver

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import express, {Request, Response, NextFunction} from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'sucesso total! vocês são demais'});
+});
+
+app.listen(3000, () => {
+    console.log('Aplicacao executando na porta 3000!');
+});
